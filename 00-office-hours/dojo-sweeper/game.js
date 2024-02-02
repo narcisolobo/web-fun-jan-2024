dc6de3754b8bc732ee1ffbba7d5a1e4cd658de91
narcisolobo <narcisolobo@gmail.com>
2024-02-01 16:25:54 -0800

    last tweak

--- 00-office-hours/dojo-sweeper/game.js
+++ 00-office-hours/dojo-sweeper/game.js
@@ -10,6 +10,7 @@ var theDojo = [
   [5, 2, 0, 2, 0, 0, 0, 1, 1, 2],
   [9, 2, 2, 2, 0, 7, 0, 1, 1, 0],
 ];
+
 var dojoDiv = document.querySelector('#the-dojo');
 
 // Creates the rows of buttons for this game
@@ -28,6 +29,7 @@ function render(theDojo) {
 //        Use i and j as the indexes to check theDojo.
 function howMany(i, j, element) {
   console.log({ i, j });
+  console.log(element);
   alert('TODO - determine how many ninjas are hiding in adjacent squares');
 }
 
