0b05354742da9e148e6072e531c9c486c286295d
narcisolobo <narcisolobo@gmail.com>
2024-02-14 18:06:24 -0800

    finished hangman

--- 12-extras/hangman/js/script.mjs
+++ 12-extras/hangman/js/script.mjs
@@ -5,10 +5,54 @@ import { alphabet, words } from './data.mjs';
 
 // references
 const playButton = document.getElementById('play');
+const buttonsDiv = document.getElementById('buttons');
+const hangmanImg = document.getElementById('hangman');
+const blanks = document.getElementsByClassName('blank');
 
 // game
 function startGame() {
-  // your code here
+  // tally variables
+  let turnCount = 0;
+  let goodGuessCount = 0;
+  let badGuessCount = 0;
+
+  clearBlanks(blanks);
+  updateHangman(hangmanImg, 0);
+  playButton.remove();
+  buttonsDiv.innerHTML = buildButtons(alphabet);
+  const word = getWord(words);
+  console.log(word);
+
+  alphabet.forEach(function (char) {
+    const button = document.getElementById(char);
+
+    button.addEventListener('click', function () {
+      const letter = getLetter(button);
+      console.log(letter);
+      turnCount++;
+
+      button.classList.add('disabled');
+
+      if (!word.includes(letter)) {
+        badGuessCount++;
+        updateHangman(hangmanImg, badGuessCount);
+        if (badGuessCount >= 7) {
+          gameOver(buttonsDiv, word);
+        }
+      } else {
+        const wordArray = word.split('');
+        for (let i = 0; i < wordArray.length; i++) {
+          if (wordArray[i] === letter) {
+            blanks[i].value = letter;
+            goodGuessCount++;
+          }
+        }
+        if (goodGuessCount >= 6) {
+          youWin(buttonsDiv, hangmanImg);
+        }
+      }
+    });
+  });
 }
 
 playButton.addEventListener('click', startGame);
